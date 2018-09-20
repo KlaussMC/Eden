10cfafba8a1c8a9bdca71a0ac52e8af0b48c7ad1
Jakes-PC\Jacob Schneider <jakieschneider13@gmail.com>
2018-09-20 21:21:02 +1000

    Fine tuned terrain gen

--- src/Terrain.java
+++ src/Terrain.java
@@ -4,14 +4,25 @@ import java.awt.*;
 
 public class Terrain {
 
-    public static Color[] colors = {new Color(142, 78, 5), new Color(28, 142, 5), new Color(5, 84, 142), new Color(198, 182, 14)};
+    public static Color[] colors = {
+            new Color(142, 78, 5),
+            new Color(142, 78, 5),
+            new Color(28, 142, 5),
+            new Color(28, 142, 5),
+            new Color(5, 84, 142),
+            new Color(198, 182, 14),
+            new Color(198, 182, 14),
+            new Color(56, 58, 71),
+            new Color(56, 58, 71)
+    };
+
     public static boolean[] solids = {true, true, false, true};
 
     public static int[][] generateTerrain(int width, int height, PApplet p) {
 
         int[][] terrain = new int[width][height];
 
-        float increment = 0.02f;
+        float increment = 0.01f; // this number is variation. the bigger it is, the more change in terrain.
         float xoff = 0.0f;
 
         for (int i = 0; i < width; i++) {
@@ -21,7 +32,7 @@ public class Terrain {
             for (int j = 0; j < height; j++) {
                 yoff += increment;
                 float n = p.noise(xoff, yoff);
-                int number = p.floor(n * 4);
+                int number = p.floor(n * colors.length);
                 terrain[i][j] = number;
             }
         }
